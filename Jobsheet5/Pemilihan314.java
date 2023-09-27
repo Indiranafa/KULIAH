0ee12a86c4450ca139caae56070bfde71473f5a1
Indira Nafa <indiranafa24@gmail.com>
2023-09-27 12:57:15 +0700

    Belajar Switch Case

--- Jobsheet5/Pemilihan314.java
+++ Jobsheet5/Pemilihan314.java
@@ -18,19 +18,19 @@ public class Pemilihan314 {
         switch (operator) {
             case '+':
                 hasil = angka1 + angka2;
-                System.out.println(angka1 + " + " + angka2 + "=" + hasil);
-                break;
+                System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+                //break;
             case '-':
                 hasil = angka1 - angka2;
-                System.out.println(angka1 + " - " + angka2 + "=" + hasil);
+                System.out.println(angka1 + " - " + angka2 + " = " + hasil);
                 break;
             case '*':
                 hasil = angka1 * angka2;
-                System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+                System.out.println(angka1 + " * " + angka2 + " = " + hasil);
                 break;
             case '/':
                 hasil = angka1 / angka2;
-                System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+                System.out.println(angka1 + " / " + angka2 + " = " + hasil);
                 break;
             default:
                 System.out.print("Salah");
