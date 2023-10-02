e600189f921e8cdab5fa8a02a276971dbaec8cbf
Indira Nafa <indiranafa24@gmail.com>
2023-10-02 15:00:34 +0700

    Update Pemilihan2114

--- Jobsheet6/Pemilihan2114.java
+++ Jobsheet6/Pemilihan2114.java
@@ -19,11 +19,17 @@ public class Pemilihan2114 {
         // System.out.println("Bukan Tahun Kabisat");
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0) {
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
             }else{
-                System.out.println("Bukan Tahun Kabisat");
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                }else{
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
         }
    }
 }
